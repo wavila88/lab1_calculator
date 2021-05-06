dd6686cc2366a42f46001e1f9463cb018470a9fd
William Avila <willocoav@example.com>
2021-05-05 19:58:04 -0500

    added unit test

--- src/test/java/com/hades/labs/calculator/ApplicationTests.java
+++ src/test/java/com/hades/labs/calculator/ApplicationTests.java
@@ -1,13 +1,22 @@
 package com.hades.labs.calculator;
 
+import com.hades.labs.calculator.controller.CalculatorController;
+import com.hades.labs.calculator.service.CalculatorService;
+import com.hades.labs.calculator.service.implement.CalculatorServiceImpl;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 @SpringBootTest
 class ApplicationTests {
 
+	private CalculatorServiceImpl calculatorServiceImpl = new CalculatorServiceImpl();
 	@Test
 	void contextLoads() {
+		Double response = calculatorServiceImpl.sum(1d,2d);
+		Assert.assertEquals(java.util.Optional.of(response), java.util.Optional.of(3d));
 	}
 
 }
